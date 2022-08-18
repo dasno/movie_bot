e70756c1ccbe8257b0b5b9a05bdbc9f70b6a7778
Daniel Pohancanik <danielpohancanik@protonmail.com>
2022-08-18 16:28:09 +0200

    get libs, lib items functions

---
+++ Classes/LibraryItems.py
@@ -0,0 +1,81 @@
+from typing import List
+from typing import Any
+from dataclasses import dataclass
+
+#GENERATED FROM https://json2csharp.com/
+
+@dataclass
+class UserData:
+    PlaybackPositionTicks: str
+    PlayCount: str
+    IsFavorite: str
+    Played: str
+    Key: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'UserData':
+        _PlaybackPositionTicks = str(obj.get("PlaybackPositionTicks"))
+        _PlayCount = str(obj.get("PlayCount"))
+        _IsFavorite = str(obj.get("IsFavorite"))
+        _Played = str(obj.get("Played"))
+        _Key = str(obj.get("Key"))
+        return UserData(_PlaybackPositionTicks, _PlayCount, _IsFavorite, _Played, _Key)
+
+@dataclass
+class Item:
+    Name: str
+    ServerId: str
+    Id: str
+    HasSubtitles: str
+    Container: str
+    PremiereDate: str
+    CriticRating: str
+    OfficialRating: str
+    ChannelId: str
+    CommunityRating: str
+    RunTimeTicks: str
+    ProductionYear: str
+    IsFolder: str
+    Type: str
+    UserData: UserData
+    VideoType: str
+    LocationType: str
+    MediaType: str
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'Item':
+        _Name = str(obj.get("Name"))
+        _ServerId = str(obj.get("ServerId"))
+        _Id = str(obj.get("Id"))
+        _HasSubtitles = str(obj.get("HasSubtitles"))
+        _Container = str(obj.get("Container"))
+        _PremiereDate = str(obj.get("PremiereDate"))
+        _CriticRating = str(obj.get("CriticRating"))
+        _OfficialRating = str(obj.get("OfficialRating"))
+        _ChannelId = str(obj.get("ChannelId"))
+        _CommunityRating = str(obj.get("CommunityRating"))
+        _RunTimeTicks = str(obj.get("RunTimeTicks"))
+        _ProductionYear = str(obj.get("ProductionYear"))
+        _IsFolder = str(obj.get("IsFolder"))
+        _Type = str(obj.get("Type"))
+        _UserData = UserData.from_dict(obj.get("UserData"))
+        _VideoType = str(obj.get("VideoType"))
+        _LocationType = str(obj.get("LocationType"))
+        _MediaType = str(obj.get("MediaType"))
+        return Item(_Name, _ServerId, _Id, _HasSubtitles, _Container, _PremiereDate, _CriticRating, _OfficialRating, _ChannelId, _CommunityRating, _RunTimeTicks, _ProductionYear, _IsFolder, _Type, _UserData, _VideoType, _LocationType, _MediaType)
+
+
+@dataclass
+class LibraryItems:
+    Items: List[Item]
+
+    @staticmethod
+    def from_dict(obj: Any) -> 'LibraryItems':
+        _Items = [Item.from_dict(y) for y in obj.get("Items")]
+        return LibraryItems(_Items)
+
+
+
+# Example Usage
+# jsonstring = json.loads(myjsonstring)
+# root = Root.from_dict(jsonstring)
--- jellyapiclient.py
+++ jellyapiclient.py
@@ -1,10 +1,56 @@
+from ast import keyword
+from sqlite3 import connect
+from xml.etree.ElementTree import tostring
+from Classes.Connection import Connection
+from Classes.AllLibs import AllLibs
+from Classes.LibraryItems import LibraryItems
 import json
 import requests
+from dotenv import load_dotenv
+import os
 
-API_KEY = '48ed9a373062474a8ed04a4a8602b600'
-header = {'X-Emby-Token':API_KEY}
-response = requests.get('http://192.168.1.3:8096/Items',headers=header, params={'userId':'e726513ac7224e8895ea76e5d5028aa1','parentId':'f137a2dd21bbc1b99aa5c0f6bf02a805'})
-print(response.status_code)
-print(response.url)
-print(response.headers)
-print(response.content)
+load_dotenv()
+API_KEY = os.getenv('JELLY_KEY')
+ADDRESS = os.getenv('ADDRESS')
+#TODO get all defualt bot userId from env file
+
+
+con = Connection(API_KEY, ADDRESS)
+
+def GetAllLibs(connection: Connection):
+    header = {'X-Emby-Token':API_KEY}
+    target = connection.Address + "/Items"
+    response = requests.get(target,
+                            headers=header,
+                            params={'userId':'2ae057263010412386af9d21168de268'})
+    resJson = json.loads(response.content)
+    return AllLibs.from_dict(resJson)
+
+
+def GetLibraryItems(connection: Connection, libId):
+    header = {'X-Emby-Token':API_KEY}
+    target = connection.Address + "/Items"
+    response = requests.get(target,
+                            headers=header,
+                            params= {'userId':'2ae057263010412386af9d21168de268',
+                                    'parentId' : libId})
+    resJson = json.loads(response.content)
+    return LibraryItems.from_dict(resJson)
+
+def GetLibByName(con:Connection, libname):
+    libs = GetAllLibs(con)
+    lib = None
+    for i in libs.Items:
+        if(i.Name == libname):
+            lib = i
+            break
+    return lib
+
+
+
+lib = GetLibByName(con, "Movies")
+items = GetLibraryItems(con, str(lib.Id))
+for i in items.Items:
+    print(i.Name)
+
+        
