38332c429a4f310ad46a432b239cf8cb9435a19f
Daniel Pohancanik <danielpohancanik@protonmail.com>
2023-03-02 12:53:16 +0100

    added timezone info to f1 when output, CET

--- formula.py
+++ formula.py
@@ -1,19 +1,18 @@
 from Classes.Calendar import Calendar
-import json
 from datetime import datetime, timedelta, timezone
-from typing import List, Any
-from dateutil.parser import isoparse
+from typing import Any
+import pytz
 
 class FormulaFeature():
 
-    #Calendar.GPs[0].Sessions[0].StartTime.st
     @staticmethod
+    
     def FindClosestSession(data:Any):
-        data = Calendar.from_dict(data)
-        for x in data.GPs:
+        parsed = Calendar.from_dict(data)
+        for x in parsed.GPs:
             for y in x.Sessions:
                 if (y.StartTime) >= datetime.now(timezone.utc):
-                    return "Next session is: " + x.Name + " - " + y.Name + " @ " + y.StartTime.strftime("%d.%m.%Y %H:%M")
+                    return "Next session is: " + x.Name + " - " + y.Name + " @ " + y.StartTime.astimezone(pytz.timezone('Europe/Bratislava')).strftime("%d.%m.%Y %H:%M %Z")
             
 
 
