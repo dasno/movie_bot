773d558a1fa20ea3fef6d3e06533134ffbd6e583
Daniel Pohancanik <danielpohancanik@protonmail.com>
2023-03-04 15:41:26 +0100

    settings from a file, disabled stream checking

--- bot.py
+++ bot.py
@@ -10,14 +10,52 @@ import json
 from typing import Literal, List
 import streams
 from discord.ext import tasks
+import configparser
+import subprocess
+import asyncio
 
-load_dotenv()
+"""load_dotenv()
 API_KEY = os.getenv('JELLY_KEY')
 ADDRESS = os.getenv('ADDRESS')
 JELLY_UID = os.getenv('JELLY_BOT_UID')
 TOKEN = os.getenv('BOT_TOKEN')
 SERVER_ID = os.getenv('DISCORD_SERVER_ID')
-STREAM_URL = "rtmp://192.168.1.3/live/obs_stream"
+STREAM_URL = "rtmp://192.168.1.3/live/obs_stream"""
+
+
+
+
+def CreateSettingsIni():
+    try:
+        file = open("settings.conf", "x")
+    except:
+        print("Settings found.")
+        return
+    filetext = "[BOT]\nDiscordToken =\nJellyApi =\nJellyAddress =\nBot_UID =\nDiscordServerID =\nStreamIP= "
+    file.write(filetext)
+    print("File created. Fill the settings file.")
+    exit(2)
+
+def LoadSettings():
+    config = configparser.ConfigParser()
+    config.read('settings.conf')
+    config.sections()
+    return config
+
+CreateSettingsIni()
+settings = LoadSettings()
+
+API_KEY = settings['BOT']['JellyApi']
+ADDRESS = settings['BOT']['JellyAddress']
+JELLY_UID = settings['BOT']['Bot_UID']
+TOKEN = settings['BOT']['DiscordToken']
+SERVER_ID = settings['BOT']['DiscordServerID']
+STREAM_URL = settings['BOT']['StreamIP']
+
+
+if not API_KEY or not ADDRESS or not JELLY_UID or not TOKEN or not SERVER_ID or not STREAM_URL:
+    print("Missing settings values")
+    exit(3)
 
 
 intents = discord.Intents.default()
@@ -104,21 +142,24 @@ async def F1WhenAutocomplete(
         ]
 
 
-@tasks.loop(seconds=5)
+"""@tasks.loop(seconds=20)
 async def RunStreamCheck():
     print("Looping")
-    if streams.CheckStreamRunning(STREAM_URL):
+    res = await asyncio.create_subprocess_shell("timeout 10s ffprobe -v quiet {STREAM_URL}")
+    print("return: "+str(res.returncode))
+    
+    if not res.returncode:
         activity = discord.Streaming(name="STREAM ON", url="http://192.168.196.100:8096")
         await client.change_presence(activity=activity)
     else:
-       await client.change_presence(activity=None) 
+       await client.change_presence(activity=None)"""
 
 @client.event
 async def on_ready():
     await tree.sync(guild=discord.Object(id=SERVER_ID))
     print(f'We have logged in as {client.user}')
     print("Ready!")
-    RunStreamCheck.start()
+    #RunStreamCheck.start()
    
 #reactions to messages without slash    
 @client.event
