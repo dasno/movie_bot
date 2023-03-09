13042267c9c12dc5717409f2dc77bbb06c3010e5
Daniel Pohancanik <danielpohancanik@protonmail.com>
2023-03-09 19:06:49 +0100

    fixed exception when getting GP info

--- bot.py
+++ bot.py
@@ -89,7 +89,7 @@ async def F1Command(interaction, option:str, option2:str=None):
         sessionString = ""
 
         for x in res.Sessions:
-            sessionString += "{sessionName} @ {sessionTime}\n".format(sessionName = x.Name, sessionTime = FormulaFeature.GetFormattedSessionTime(x))
+            sessionString += "{sessionName} @ {sessionTime}\n".format(sessionName = x.Name, sessionTime = FormulaFeature.GetFormattedSessionTime(x, None))
 
         response = "Round {roundnr} - {GPName} \nSessions:\n{sessionList}".format(roundnr = res.Round, GPName = res.Name, sessionList = sessionString)
         await interaction.response.send_message(response)
